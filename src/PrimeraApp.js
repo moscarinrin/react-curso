a75f588d14a208ba37f0e8b60c9a29dae3c070fd
moscarinrin <moscarin94@gmail.com>
2020-08-19 15:40:51 +0100

    react

---
+++ src/PrimeraApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+// Trabajaremos con Funcional components
+// Hay componentes de const y clases
+
+const PrimeraApp = () => {
+  return <h1> Hola Mundo </h1>
+};
+
+export default PrimeraApp;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+// Que es un componente en react?
+// Es una pequeña pieza de codigo encapsulada reutilizable que puede tener estado o nelpas
+// El comcepto de estado: Es el valor del componente en algun momento del tiempo
+import React from "react";
+import ReactDoom from "react-dom"
+import PrimeraApp from "./PrimeraApp";
+import "./index.css";
+
+// Variable que es renderizada en elemento de public
+
+
+// const saludo = <h1>Hola mundo</h1>;
+
+// const divRoot = document.querySelector("#root")
+
+// ReactDoom.render(saludo, divRoot);
+// Codigo
+
+//Componente
+
+const divRoot = document.querySelector("#root")
+
+ReactDoom.render(<PrimeraApp /> , divRoot);
